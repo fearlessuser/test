52e37a95f8cf3c21a7f579ef91a69400fc6ad2b1
zhangbenjing <209218780@qq.com>
2024-08-30 12:39:41 +0800

    删除了test.c中的注释

--- test/test.c
+++ test/test.c
@@ -1,25 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-// int main()
-// {
-//     int n;
-//     scanf("%d",&n);
-//     int dp[n+1];
-//     memset(dp,0,sizeof(dp));
-//     dp[0] = 1;
-//     dp[1] = 1;
-//     for(int i = 2;i<=n;i++)
-//     {
-//         int sum = 0;
-//         for(int j = 0;j<i;j++)
-//         {
-//             sum += dp[j]*dp[i-j-1];
-//         }
-//         dp[i] = sum;
-//     }
-//     printf("%d",dp[n]);
-//     return 0;
-// }
 int main()
 {
     int i = 0;
